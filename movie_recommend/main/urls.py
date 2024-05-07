4ee6b0b3d9b7104c054f6149b42a1b862ed11b17
trungviet17 <113108053+trungviet17@users.noreply.github.com>
2024-05-07 23:24:07 +0700

    update html

--- movie_recommend/main/urls.py
+++ movie_recommend/main/urls.py
@@ -8,5 +8,5 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'), 
     path('search/', views.search, name='search'), 
-    path('recommend/<int:movie_id>', views.recommend, name='recommend'),
+    path('recommend/<int:movie_id>/', views.recommend, name='recommend'),
 ]
